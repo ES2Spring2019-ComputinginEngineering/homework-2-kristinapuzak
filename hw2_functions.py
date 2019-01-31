9dc03afe52fd23339df2a7a91f59636bf112e12b
Kristina Puzak <kristina.puzak@tufts.edu>
2019-01-30 21:24:43 -0500

    Update hw2_functions.py

--- hw2_functions.py
+++ hw2_functions.py
@@ -3,10 +3,10 @@
 
 # FILL THESE COMMENTS IN
 #*****************************************
-# YOUR NAME:
-# NUMBER OF HOURS TO COMPLETE:  (please track how long this homework takes you to complete).
+# YOUR NAME: Kristina Puzak
+# NUMBER OF HOURS TO COMPLETE: 1-2 (please track how long this homework takes you to complete).
 # YOUR COLLABORATION STATEMENT(s) (refer to syllabus):
-#
+#I received assistance from Bo on this assignment
 #*****************************************
 
 #In this homework,the ultimate goal is to create a function called areaofatriangle,
@@ -14,11 +14,11 @@
 #of the form y = (m * x) + b that mark the three sides of the triangle.
 
 #In order to accomplish this you will need functions which determine
-#where two lines intersect (x and y), a function which determines the distance between 
+#where two lines intersect (x and y), a function which determines the distance between
 #two points represented by (x,y) coordinates, and a function which determines
-#the area of a triangle using three side lengths(using Heron's Formula). 
+#the area of a triangle using three side lengths(using Heron's Formula).
 
-#Please complete the four required functions below:  
+#Please complete the four required functions below:
 
 import math #This line allows you to use math functions. Importantly, math.sqrt(#) which will produce the square root of the number inside the parentheses.
 
@@ -26,47 +26,55 @@ import math #This line allows you to use math functions. Importantly, math.sqrt(
 def intersectionoftwolines_x(m1, b1, m2, b2):
     # Calculate x for the point where two equations:
     # y = (m1 * x) + b1 and y = (m2 * x) + b2 intersect.
-    
-    
-    x = 0 #replace this with your calculation for x
+
+
+    x = (b2 - b1) / (m1 - m2) #replace this with your calculation for x
     return x
 
 def intersectionoftwolines_y(m1, b1, m2, b2):
     # Calculate y for the point where two equations:
     # y = (m1 * x) + b1 and y = (m2 * x) + b2 intersect.
-    
-    
-    y = 0 #replace this with your calculation for y
+
+
+    y = m1 * ((b2 - b1) / (m1  - m2)) + b1 #replace this with your calculation for y
     return y
 
 
 def distancebetweenpoints(x1, y1, x2, y2):
     # Calculate the linear distance between two points
     # (x1, y1) and (x2, y2).
-    
-    
-    distance = 0 # replace with your calculation for distance
+
+
+    distance = math.sqrt(((x2 - x1)**2) + ((y2 - y1)**(2))) # replace with your calculation for distance
     return distance
-    
-def heronsformula(a, b, c):
+
+def heronsformula(a,b,c):
     # Calculate the area of a triangle with three known side lengths.
     # You may want to look up Heron's formula online.
-    
-    
-    
-    area = 0 #replace this with your calculation for area
+    s = (a + b + c) / 2.0
+
+
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c)) #replace this with your calculation for area
     return area
-    
+
 def areaofatriangle(m1, b1, m2, b2, m3, b3):
-    #Using the three functions above, now calculate the area of a 
+    #Using the three functions above, now calculate the area of a
     #triangle when the three sides are described by three linear equations
     #y = (m1 * x) + b1;  y = (m2 * x) + b2; and y = (m3 * x) + b3
-    
-    
-    area = 0 #replace this with your calculation for area
+    x1 = intersectionoftwolines_x(m1, b1, m2, b2)
+    y1 = intersectionoftwolines_y(m1, b1, m2, b2)
+    x2 = intersectionoftwolines_x(m2, b2, m3, b3)
+    y2 = intersectionoftwolines_y(m2, b2, m3, b3)
+    x3 = intersectionoftwolines_x(m3, b3, x1, b1)
+    y3 = intersectionoftwolines_y(m3, b3, x1, b1)
+    a = distancebetweenpoints(x1, y1, x2, y2)
+    b = distancebetweenpoints(x2, y2, x3, y3)
+    c = distancebetweenpoints(x3, y3, x1, y1)
+
+    area = heronsformula(a,b,c) #replace this with your calculation for area
     return area
-    
-    
+
+
 #TEST CASES
 #These print statements will be true when your functions are working.
 
